9d4e13f8847a7cd17d2608678827e7a88e1e9ef3
Mousmi Sorathiya <sora0008@algonquinlive.com>
2023-09-26 22:32:04 -0400

    defining function

---
+++ MousmiS_NiralP/area_calculator.py
@@ -0,0 +1,12 @@
+"""This module will calculate the area of a few different shapes."""
+
+dir
+from math import pi
+def get_circle_area(radius):
+    """abs returns the absolute value of the argument"""
+    circle = pi*(radius**2)
+    return circle    
+
+def get_square_area(side):
+     square = side**2
+     return square
