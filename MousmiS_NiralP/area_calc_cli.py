51775060555174438058778b648a809361081a4c
Mousmi Sorathiya <sora0008@algonquinlive.com>
2023-09-26 22:49:46 -0400

    import function

---
+++ MousmiS_NiralP/area_calc_cli.py
@@ -0,0 +1,12 @@
+import area_calculator
+print(area_calculator.get_circle_area(2))
+
+from area_calculator import get_circle_area
+print(get_circle_area(2))
+
+from area_calculator import get_square_area
+print(get_square_area(2))
+
+import area_calculator
+print(area_calculator.get_square_area(2))
+
