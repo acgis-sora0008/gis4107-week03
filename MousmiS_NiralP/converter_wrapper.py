ae0b580a3c9786517762c41f69a235b657899a55
acgis-sora0008 <sora0008@algonquinlive.com>
2023-10-03 05:20:15 +0530

    converter_wrapper_test file

--- MousmiS_NiralP/converter_wrapper.py
+++ MousmiS_NiralP/converter_wrapper.py
@@ -1,11 +1,44 @@
 import converter
+#1
+def miles_to_km(miles):
+    print(f'{miles} miles = {converter.miles_to_km(miles)} km')
 
-print(converter.miles_to_km(5))
-print(converter.km_per_hr_to_m_per_s(56.7))
-print(converter.sqmetres_to_hectares(2232.56))
-print(converter.sqmetres_to_acres(1145.67))
-print(converter.acres_to_edge_of_square(25.13))
-print(converter.get_bear_count(10))
-print(converter.dms_to_dd(75,56,34))
-print(converter.dd_to_dms(-56.7869))
-print(converter.get_fuel_cost(35,112))
+#2
+def km_per_hr_to_m_per_s(km_per_hr):
+    print(f'{km_per_hr} km_per_hr = {converter.km_per_hr_to_m_per_s(km_per_hr):.1f} m_per_s')
+
+#3
+def sqmetres_to_hectares(sq_meters):
+    print(f'{sq_meters} sq_meters = {converter.sqmetres_to_hectares(sq_meters)} hectares')
+
+#4
+def sqmetres_to_acres(sq_meters):
+    print(f'{sq_meters} sq_meters = {converter.sqmetres_to_acres(sq_meters)} acres')
+
+#5
+def acres_to_edge_of_square(acres):
+    print(f'{acres} acres = {converter.acres_to_edge_of_square(acres)} edge_of_square')
+
+#6
+def get_bear_count(count):
+    print(f'{count} count = {converter.get_bear_count(10)} get_bear_count')
+
+#7
+def dms_to_dd(degrees, minutes, seconds):
+    print(f'{degrees, minutes, seconds} d_m_s =  {converter.dms_to_dd(degrees, minutes, seconds)} dd')
+
+#8
+def dd_to_dms(degrees):
+    print(f'{degrees} dd = {converter.dd_to_dms(degrees)} dms')
+
+#9
+def get_fuel_cost(mileage,fuel_price):
+    print(f'{mileage, fuel_price} m_fp = {converter.get_fuel_cost(mileage, fuel_price)} fuel_cost')
+
+
+
+
+
+
+
+
--- MousmiS_NiralP/coverter_wrapper_test.py
+++ MousmiS_NiralP/coverter_wrapper_test.py
@@ -1,10 +1,95 @@
 import converter_wrapper as cw
+
+#1
 def test_miles_to_km():
     miles = 10
     actual = cw.miles_to_km(miles)
     expected = '10 mi = 16.0934 km'
     print(f'Expected : {expected}')
     print(f'actual :{actual}')
-
     test_miles_to_km()
+    print('')
+
+#2
+def test_km_per_hr_to_m_per_s():
+    km_per_hr = 100
+    actual = cw.km_per_hr_to_m_per_s(km_per_hr)
+    expected = '100 km/hr = 27.8 m/s'
+    print(f'Expected: {expected}')
+    print(f'Actual: {actual}')
+    test_km_per_hr_to_m_per_s()
+    print('')
+
+#3
+def test_sqmetres_to_hectares():
+    sq_meters = 10000
+    actual = cw.sqmetres_to_hectares(sq_meters)
+    expected = '10000 sqm = 1 hectare'
+    print(f'Expected: {expected}')
+    print(f'Actual: {actual}')
+    test_sqmetres_to_hectares()
+    print('')
+
+#4
+def test_sqmetres_to_acres():
+    sq_meters = 4000
+    actual = cw.sqmetres_to_acres(sq_meters)
+    expected = '4000 sqm = 0.988 acre'
+    print(f'Expected: {expected}')
+    print(f'Actual: {actual}')
+    test_sqmetres_to_acres()
+    print('')
+
+#5
+def test_acres_to_edge_of_square():
+    acres = 1
+    actual = cw.acres_to_edge_of_square(acres)
+    expected = '1 acre = 4046.84 sqm  edge_length = sqrt(4046.84) = 63.615'
+    print(f'Expected: {expected}')
+    print(f'Actual: {actual}')
+    test_acres_to_edge_of_square()
+    print('')
+
+#6
+def test_get_bear_count():
+    count = 10
+    actual = cw.get_bear_count(count)
+    expected = ' g_b_c = 10 '
+    print(f'Expected: {expected}')
+    print(f'Actual: {actual}')
+    test_get_bear_count()
+    print('')
+
+
+#7
+def test_dms_to_dd():
+    degrees = 25
+    minutes = 55
+    seconds = 34
+    actual = cw.dms_to_dd(degrees, minutes, seconds)
+    expected =  ' dms(75,56,34) = 75.9428 dd '
+    print(f'Expected: {expected}')
+    print(f'Actual: {actual}')
+    test_dms_to_dd()
+    print('')
+
+#8
+def test_dd_to_dms():
+    degrees = 25.13
+    actual = cw.dd_to_dms(degrees)
+    expected =  'dd 25.13 = dms(25,7,48) '
+    print(f'Expected: {expected}')
+    print(f'Actual: {actual}')
+    test_dd_to_dms()
+    print('')
+
+#9
+def test_get_fuel_cost():
+    mileage = 35
+    fuel_price = 112
+    actual = cw.get_fuel_cost(mileage, fuel_price)
+    expected = ' fuel_cost = 35 * 112 = 3920 '
+    print(f'Expected: {expected}')
+    print(f'Actual: {actual}')
+    test_get_fuel_cost()
     print('')
